e8eb23d9f87a91469b16306eb1c7ac80c441b408
Administrator <Administrator@WIN-7D2IAMHC3IG>
2024-10-13 14:24:07 +0800

    Add project files.

---
+++ MyThreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace frmBasicThread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int t1 = 0; t1 <= 5; t1++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + t1);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
---
+++ frmBasicThread.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmBasicThread
+{
+    public partial class frmBasicThread : Form
+    {
+        public frmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Before starting thread-");
+            label1.Text = "----Before starting thread----";
+
+            ThreadStart thread1 = new ThreadStart(MyThreadClass.Thread1);
+
+
+            Thread ThreadA = new Thread(thread1);
+            Thread ThreadB = new Thread(thread1);
+
+            ThreadA.Name = "Thread A Process";
+            ThreadB.Name = "Thread B Process";
+
+            ThreadA.Priority = ThreadPriority.Normal;
+            ThreadB.Priority = ThreadPriority.Highest;
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            Console.WriteLine("End of Thread");
+            label1.Text = "End of Thread";
+
+        }
+
+        private void frmBasicThread_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
